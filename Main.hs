05b5207bda09f632196bc84c6a144270c5778c35
Alessandro Marrella <dev@alessandromarrella.com>
2021-01-02 11:22:19 +0000

    add examples

--- Main.hs
+++ Main.hs
@@ -1,4 +1,28 @@
+{-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE QualifiedDo #-}
+
 module Main where
 
+import qualified System.IO.Resource as Linear
+import qualified Control.Functor.Linear as Control
+import qualified Data.Text as Text
+import Prelude.Linear
+import qualified Prelude
+import qualified System.IO
+import Control.Monad (void)
+
+id' :: a %1-> a
+id' x = x
+
+writeToFile' :: String -> IO ()
+writeToFile' x = 
+  Linear.run Prelude.$ Control.do 
+    h1 <- Linear.openFile "tmp/test.txt" Linear.WriteMode
+    h2 <- Linear.hPutStrLn h1 (Text.pack x)
+    () <- Linear.hClose h2
+    Control.return (Ur ())
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = writeToFile' "Hello, Haskell!"
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Learning how to use linear types
 
 Repo derived from: https://github.com/tweag/linear-base
 
+To get the repo's GHC version, `cachix use amarrella`, unless you like compiling for hours
